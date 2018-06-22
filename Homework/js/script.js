3c07b653bf95917c8b15aecad821b30c9c1455a4
Davit <davit.hovhannisyan.91@gmail.com>
2018-06-22 19:49:20 +0400

    Table

---
+++ Homework/js/script.js
@@ -0,0 +1,108 @@
+
+
+window.onload = function(){
+
+
+   class A {   // class singleton
+      constructor(){
+        if(A.instance){
+          return A.instance
+        }else {
+          A.instance = this;
+        }
+      }
+    }
+
+
+    let names = ['Susanna', 'Vardan', 'Armen', 'Gevorg', 'Babken', 'Gayane'];
+
+    
+
+
+
+    class User {
+
+        constructor(firstName, lastName, age, email, password){
+            // this.id = getID();
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.email = email;
+            this.password = password;
+            // this.table = table;
+        }
+
+        addToTable(){
+            let table = document.getElementsByTagName('table')[0],
+                row = document.createElement('tr'),
+                cell, input;
+                cell = document.createElement('td');
+                cell.innerHTML = getId();
+                input = document.createElement('input');
+                input.setAttribute("type", 'checkbox');
+                input.style.float = 'left'
+                cell.appendChild(input);
+                
+                row.appendChild(cell);
+
+            for(let prop in this) {
+                
+
+               cell = document.createElement('td');
+               input = document.createElement('input');
+
+               input.style.width = this[prop].toString().length  * 9 + 'px';
+               input.value = this[prop];
+               
+               cell.appendChild(input);
+               row.appendChild(cell);
+               table.appendChild(row);
+
+               cell.onclick = function(){
+                   this.children[0].focus();
+               }
+
+               input.onkeyup = function(){
+                   this.style.width = this.value.length * 9 + 'px'
+               }
+
+            }
+        }
+
+    }
+
+    let i = 1;
+    function getId() {
+        
+        return i++
+    }
+
+
+        let cellesDom = document.getElementsByTagName('td'),
+        data = [
+                    new User('Armen', 'Gevorgyan', 19, 'e-mail', 'password').addToTable(),
+                    new User('Karen', 'Vardanyan', 47, 'e-mail', 'password').addToTable(),
+                    new User('Gevorg', 'Avetisyan', 19, 'e-mail', 'password').addToTable()
+                ];
+
+        
+
+        
+
+    
+    let buttons = document.getElementsByClassName('button');
+
+    for(let i = 0; i < buttons.length; i++) {
+        buttons[i].onclick = function(){
+            this.style.boxShadow = 'none';
+            let self = this;
+            setTimeout(function(){
+                self.style.boxShadow = '1px 2px #777';
+            }, 120)
+        }
+    }
+  
+}
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 #learning js
-#Learning js
+
